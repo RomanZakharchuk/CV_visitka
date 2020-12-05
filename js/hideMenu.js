666452884340b77569cbcee664df37516d090db4
Rombo <romanzakharchuk32@gmail.com>
2020-12-05 18:50:27 +0200

    add burger

---
+++ js/hideMenu.js
@@ -0,0 +1,4 @@
+$('.flowing-scroll').on('click', function () {
+    $('.navigation').removeClass('open-menu');
+    $('.menu-burger_header').removeClass('open-menu');
+});
---
+++ js/menu.js
@@ -0,0 +1,4 @@
+$('.menu-burger_header').click(function () {
+    $('.menu-burger_header').toggleClass('open-menu');
+    $('.navigation').toggleClass('open-menu');
+});
